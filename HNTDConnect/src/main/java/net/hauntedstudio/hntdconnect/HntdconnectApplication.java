25e9db3ac3995d25cd47b6f358a7ba5b85da8dab
UAArthur <shadowmerklingerarthur@gmail.com>
2025-10-03 15:08:12 +0200

    Set up initial project structure for HNTDConnectServer application

---
+++ HNTDConnect/src/main/java/net/hauntedstudio/hntdconnect/HntdconnectApplication.java
@@ -0,0 +1,13 @@
+package net.hauntedstudio.hntdconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HntdconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HntdconnectApplication.class, args);
+	}
+
+}
